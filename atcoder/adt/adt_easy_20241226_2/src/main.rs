93eb484f0f6c0533dc2f56ad907dc326bc4f7c54
Stefan Utamaru <github.le5ke@stenyan.jp>
2024-12-26 18:42:30 +0900

    B問題

--- atcoder/adt/adt_easy_20241226_2/src/main.rs
+++ atcoder/adt/adt_easy_20241226_2/src/main.rs
@@ -1,4 +1,6 @@
-// Result: https://atcoder.jp/contests/adt_easy_20241226_2/submissions/61098592
+// Result:
+// https://atcoder.jp/contests/adt_easy_20241226_2/submissions/61098592
+// https://atcoder.jp/contests/adt_easy_20241226_2/submissions/61098746
 
 // https://qiita.com/penguinshunya/items/cd96803b74635aebefd6
 fn read<T: std::str::FromStr>() -> T {
@@ -7,7 +9,7 @@ fn read<T: std::str::FromStr>() -> T {
     s.trim().parse().ok().unwrap()
 }
 
-fn solve_a() {
+fn _solve_a() {
     let n: String = read();
 
     let ones = n.chars().filter(|&c| c == '1').count();
@@ -21,6 +23,20 @@ fn solve_a() {
     }
 }
 
+fn solve_b() {
+    let _ = read::<i32>();
+    let s = read::<String>();
+
+    let ok_count = s.chars().filter(|&c| c == 'o').count();
+    let ng_count = s.chars().filter(|&c| c == 'x').count();
+
+    if ok_count > 0 && ng_count == 0 {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
+
 fn main() {
-    solve_a();
+    solve_b();
 }
